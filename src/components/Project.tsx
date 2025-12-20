8648e4a3ac707024f3de139d4239b6b61ae6f063
羅偉駿 <weichun.lo@K2811.local>
2025-12-20 18:48:50 +0800

    Update project grid to 3 columns and standardize image display with a new wrapper for consistent sizing and centering.

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,12 +9,12 @@ function Project() {
         <h1>Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://parkfinder.streamlit.app/" target="_blank" rel="noreferrer"><img src={parking1} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://parkfinder.streamlit.app/" target="_blank" rel="noreferrer" className="project-img-wrapper"><img src={parking1} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://parkfinder.streamlit.app/" target="_blank" rel="noreferrer"><h2>ParkFinder</h2></a>
                 <p>Real-time query, tracking, and statistical analysis of available parking spaces in Taipei City. We retrieve parking data, aggregate availability for fixed time periods, and offer interactive visualization tools.</p>
             </div>
             <div className="project">
-                <a href="https://docs.google.com/spreadsheets/d/1IcTCgwnIk_EKnqRdBYK7-MGfxiTrxbTnm3-89Fc76X4/edit?usp=sharing" target="_blank" rel="noreferrer"><img src={totalResult} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://docs.google.com/spreadsheets/d/1IcTCgwnIk_EKnqRdBYK7-MGfxiTrxbTnm3-89Fc76X4/edit?usp=sharing" target="_blank" rel="noreferrer" className="project-img-wrapper"><img src={totalResult} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://docs.google.com/spreadsheets/d/1IcTCgwnIk_EKnqRdBYK7-MGfxiTrxbTnm3-89Fc76X4/edit?usp=sharing" target="_blank" rel="noreferrer"><h2>Sports Betting Freerider</h2></a>
                 <p>Elevate the Betting Game with Data-Backed Strategies. A web scraping system that captures prediction results from top-ranking players and provides statistical analyses of prediction accuracy.</p>
             </div>
