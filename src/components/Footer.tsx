fa38c528f11e79abf085e43640dc1b4681014d9b
羅偉駿 <weichun.lo@K2811.local>
2026-01-10 17:58:35 +0800

    Add resume logo link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PortraitIcon from '@mui/icons-material/Portrait';
 import { FaMedium } from 'react-icons/fa6';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useLanguage } from "../contexts/LanguageContext";
@@ -8,6 +9,7 @@ import { translations } from "../assets/data/translations";
 import '../assets/styles/Footer.scss'
 
 const FaMediumIcon = FaMedium as any;
+const ImProfileIcon = PortraitIcon as any;
 
 
 function Footer() {
@@ -32,6 +34,7 @@ function Footer() {
         <a href="https://github.com/CharlieAlex" target="_blank" rel="noreferrer"><GitHubIcon sx={{ fontSize: 28 }}/></a>
         <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"><LinkedInIcon sx={{ fontSize: 28 }}/></a>
         <a href="https://medium.com/@jasonlo1468" target="_blank" rel="noreferrer"><FaMediumIcon size={28}/></a>
+        <a href={process.env.PUBLIC_URL + (language === 'en' ? "/cv/myCV_en.pdf" : "/cv/myCV.pdf")} target="_blank" rel="noreferrer"><ImProfileIcon size={28}/></a>
       </div>
       <p>{t_footer.contact_me}charliealex0101@gmail.com</p>
     </footer>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PortraitIcon from '@mui/icons-material/Portrait';
 import { FaMedium } from 'react-icons/fa6';
 import '../assets/styles/Main.scss';
 import { useLanguage } from "../contexts/LanguageContext";
 import { translations } from "../assets/data/translations";
 
 const FaMediumIcon = FaMedium as any;
+const ImProfileIcon = PortraitIcon as any;
 
 
 function Main() {
@@ -24,6 +26,7 @@ function Main() {
             <a href="https://github.com/CharlieAlex" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
             <a href="https://medium.com/@jasonlo1468" target="_blank" rel="noreferrer"><FaMediumIcon/></a>
+            <a href={process.env.PUBLIC_URL + (language === 'en' ? "/cv/myCV_en.pdf" : "/cv/myCV.pdf")} target="_blank" rel="noreferrer"><ImProfileIcon/></a>
           </div>
           <h1>{t.name}</h1>
           <p>{t.title}</p>
@@ -33,6 +36,7 @@ function Main() {
             <a href="https://github.com/CharlieAlex" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/feed/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
             <a href="https://medium.com/@jasonlo1468" target="_blank" rel="noreferrer"><FaMediumIcon/></a>
+            <a href={process.env.PUBLIC_URL + (language === 'en' ? "/cv/myCV_en.pdf" : "/cv/myCV.pdf")} target="_blank" rel="noreferrer"><ImProfileIcon/></a>
           </div>
         </div>
       </div>
