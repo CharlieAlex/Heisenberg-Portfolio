567a2da3698fbd7ea8b21a6f3c90e14b49dae1d9
羅偉駿 <weichun.lo@K2811.local>
2026-01-10 17:22:12 +0800

    Update the layout of Honors

--- src/components/Honors.tsx
+++ src/components/Honors.tsx
@@ -64,7 +64,7 @@ function Honors() {
           </div>
         ))}
       </div>
-      <hr style={{ width: '80%', margin: '0 auto', border: '0', borderTop: '2px dashed #ccc', marginTop: '3rem' }} />
+      <hr style={{ width: '100%', margin: '0 auto', border: '0', borderTop: '2px dashed #ccc', marginTop: '3rem' }} />
     </div>
   );
 }
